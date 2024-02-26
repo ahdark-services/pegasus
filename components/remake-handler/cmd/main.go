9c8694579c9a454b6c06fe929932cdb9ce37d5b3
AH-dark <ahdark@outlook.com>
2024-02-26 23:48:45 +0800

    feat: initialized remake handler

---
+++ components/remake-handler/cmd/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+
+	"go.uber.org/fx"
+
+	"github.com/ahdark-services/pegasus/components/remake-handler/internal/bot"
+	"github.com/ahdark-services/pegasus/entry"
+)
+
+func main() {
+	app := fx.New(
+		fx.Supply(fx.Annotate(context.Background(), fx.As(new(context.Context)))),
+		fx.Supply(fx.Annotate("remake-handler", fx.ResultTags(`name:"serviceName"`))),
+
+		entry.AppEntries(),
+
+		bot.Module(),
+	)
+
+	app.Run()
+}
