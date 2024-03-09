ed76758e902173d50a3e9ffec2583eed18b30f83
AH-dark <ahdark@outlook.com>
2024-03-09 15:45:37 +0800

    feat: validate telegram secret header

--- components/gateway/internal/controllers/controller.go
+++ components/gateway/internal/controllers/controller.go
@@ -1,10 +1,13 @@
 package controllers
 
 import (
-	"github.com/ahdark-services/pegasus/components/gateway/internal/controllers/handlers"
 	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/spf13/viper"
+
+	"github.com/ahdark-services/pegasus/components/gateway/internal/controllers/handlers"
+	"github.com/ahdark-services/pegasus/components/gateway/internal/controllers/middlewares"
 )
 
-func BindRouters(svr *server.Hertz, handlers handlers.Handlers) {
-	svr.POST("/update", handlers.UpdateHandler)
+func BindRouters(svr *server.Hertz, handlers handlers.Handlers, vip *viper.Viper) {
+	svr.POST("/update", middlewares.TelegramWebhookSecretValidation(vip), handlers.UpdateHandler)
 }
---
+++ components/gateway/internal/controllers/middlewares/secret_validation.go
@@ -0,0 +1,34 @@
+package middlewares
+
+import (
+	"context"
+
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/spf13/viper"
+	"github.com/uptrace/opentelemetry-go-extra/otelzap"
+)
+
+func TelegramWebhookSecretValidation(vip *viper.Viper) app.HandlerFunc {
+	if vip.GetString("telegram.webhook.secret") == "" {
+		return func(ctx context.Context, c *app.RequestContext) {
+			otelzap.L().Ctx(ctx).Debug("Telegram webhook secret is not set")
+
+			c.Next(ctx)
+		}
+	}
+
+	return func(ctx context.Context, c *app.RequestContext) {
+		ctx, span := tracer.Start(ctx, "middlewares.TelegramWebhookSecretValidation")
+		defer span.End()
+
+		secret := c.Request.Header.Get("X-Telegram-Webhook-Secret")
+		if secret != vip.GetString("telegram.webhook.secret") {
+			c.AbortWithStatus(403)
+			otelzap.L().Ctx(ctx).Warn("Telegram webhook secret is invalid")
+			return
+		}
+
+		otelzap.L().Ctx(ctx).Debug("Telegram webhook secret is valid")
+		c.Next(ctx)
+	}
+}
