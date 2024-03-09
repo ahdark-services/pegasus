1d66234120c66f6cca9efab441e12bb144ede1ec
AH-dark <ahdark@outlook.com>
2024-03-09 15:56:40 +0800

    fix: wrong header

--- components/gateway/internal/controllers/middlewares/secret_validation.go
+++ components/gateway/internal/controllers/middlewares/secret_validation.go
@@ -2,11 +2,14 @@ package middlewares
 
 import (
 	"context"
-
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/spf13/viper"
 	"github.com/uptrace/opentelemetry-go-extra/otelzap"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
 )
+
+const TelegramBotApiSecretTokenHeader = "X-Telegram-Bot-Api-Secret-Token"
 
 func TelegramWebhookSecretValidation(vip *viper.Viper) app.HandlerFunc {
 	if vip.GetString("telegram_bot.webhook.secret_token") == "" {
@@ -21,10 +24,12 @@ func TelegramWebhookSecretValidation(vip *viper.Viper) app.HandlerFunc {
 		ctx, span := tracer.Start(ctx, "middlewares.TelegramWebhookSecretValidation")
 		defer span.End()
 
-		secret := c.Request.Header.Get("X-Telegram-Webhook-Secret")
+		secret := c.Request.Header.Get(TelegramBotApiSecretTokenHeader)
+		span.SetAttributes(attribute.String("http.header.secret_token", secret))
 		if secret != vip.GetString("telegram_bot.webhook.secret_token") {
-			c.AbortWithStatus(403)
+			span.SetStatus(codes.Error, "invalid secret token")
 			otelzap.L().Ctx(ctx).Warn("Telegram webhook secret is invalid")
+			c.AbortWithStatus(403)
 			return
 		}
 
