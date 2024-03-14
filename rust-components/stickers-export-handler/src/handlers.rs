8494ab633d1786f114cc0c029f0b72132df0cfec
AH-dark <ahdark0428@gmail.com>
2024-03-15 01:20:10 +0800

    feat: added handler for webm stickers

--- rust-components/stickers-export-handler/src/handlers.rs
+++ rust-components/stickers-export-handler/src/handlers.rs
@@ -1,11 +1,10 @@
-use std::io::Cursor;
-
-use image::ImageError;
-use image::ImageFormat::Png;
+use image::guess_format;
 use teloxide::net::Download;
 use teloxide::prelude::*;
 use teloxide::types::{InputFile, MediaKind, MessageKind};
 use teloxide::utils::command::BotCommands;
+
+use crate::convert::{convert_webm_to_gif, convert_webp_to_png};
 
 #[derive(BotCommands, Clone)]
 #[command(rename_rule = "lowercase")]
@@ -44,23 +43,40 @@ pub(crate) async fn export_sticker_handler(bot: Bot, message: Message) -> anyhow
     };
 
     // convert sticker to png
-
     let file = bot.get_file(&media_sticker.sticker.file.id).send().await?;
     let mut buffer = Vec::new();
-    bot.download_file(&file.path, &mut buffer).await?;
+    log::debug!(
+        "Downloading sticker: {}, chat id: {}",
+        file.path,
+        message.chat.id
+    );
 
-    let img = image::load_from_memory(&buffer)?;
-    let output_data = tokio::task::spawn_blocking(move || -> Result<Vec<u8>, ImageError> {
-        let mut bytes: Vec<u8> = Vec::new();
-        let mut cursor = Cursor::new(&mut bytes);
-        img.write_to(&mut cursor, Png)?;
-        Ok(bytes)
-    })
-    .await??;
+    match bot.download_file(&file.path, &mut buffer).await {
+        Ok(_) => {}
+        Err(err) => {
+            send_error_message!(
+                bot,
+                message,
+                &format!("Failed to download sticker: {}", err)
+            );
+            return Err(err.into());
+        }
+    };
+
+    let bytes = match if guess_format(&buffer).is_ok() {
+        convert_webp_to_png(&mut buffer)
+    } else {
+        convert_webm_to_gif(&mut buffer)
+    } {
+        Ok(buf) => buf,
+        Err(err) => {
+            send_error_message!(bot, message, &format!("Failed to convert sticker: {}", err));
+            return Err(err.into());
+        }
+    };
 
     // send png
-
-    bot.send_photo(message.chat.id, InputFile::memory(output_data))
+    bot.send_photo(message.chat.id, InputFile::memory(bytes))
         .reply_to_message_id(message.id)
         .send()
         .await?;
